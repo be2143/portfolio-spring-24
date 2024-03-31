c0d2eba87cae181acee5efa4a5169bd1b1348f93
buka3018 <be2143@nyu.edu>
2024-03-31 22:06:42 +0200

    general wireframe is done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Changing the word effect at the header
 // Define an array with the words to cycle through
-const words = ['Computer Science', 'Interactive Media'];
+const words = ['UI/UX Design', 'Web Development', 'Software Engineering'];
 
 // Get the span element where the text will change
 const dynamicText = document.getElementById('dynamicText');
@@ -13,23 +14,16 @@ function changeText() {
 }
 
 // Call the changeText function every 2 seconds (adjust as needed)
-setInterval(changeText, 1000);
+setInterval(changeText, 500);
 
 
-
-// Define an array with the words to cycle through
-const words1 = ['UI/UX Design', 'Web Development', 'Software Engineering'];
-
-// Get the span element where the text will change
-const dynamicText1 = document.getElementById('dynamicText');
-
-let index1 = 0;
-
-// Function to change the text
-function changeText() {
-  dynamicText.textContent = words1[index1];
-  index1 = (index1 + 1) % words1.length; // Increment index and loop back to 0 if it exceeds the length of the array
-}
-
-// Call the changeText function every 2 seconds (adjust as needed)
-setInterval(changeText, 1000);
+document.getElementById("readMore").addEventListener("click", function() {
+    var moreAbout = document.getElementById("moreAbout");
+    if (moreAbout.style.display === "none") {
+        moreAbout.style.display = "block";
+        document.getElementById("readMore").textContent = "read less";
+    } else {
+        moreAbout.style.display = "none";
+        document.getElementById("readMore").textContent = "read more";
+    }
+});
