3bca2ce3271616defd82944c1f0af90e243e4324
buka3018 <be2143@nyu.edu>
2024-04-06 04:15:28 +0200

    new design

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-document.getElementById('downloadLink').addEventListener('click', function() {
-    // Specify the path to your PDF file
-    var filePath = "images/resume.pdf";
+// Scroll back to the landing page - home (when clicked on Buka Enkhjargal)
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the header p element
+    var homeLink = document.getElementById('home-link');
 
-    // Create a link element and set its href attribute to the path of the PDF file
-    var link = document.createElement('a');
-    link.href = filePath;
+    // Add click event listener
+    homeLink.addEventListener('click', function() {
+        // Scroll to the home section
+        document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+//=======================================================================================
+// Underline the name of the current section
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('.nav-link');
 
-    // Set the download attribute of the link to specify the filename
-    link.download = 'resume.pdf';
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function() {
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+});
+//=======================================================================================
+// Apply the hover effect to the heart image when the transparent version is hovered over
+// Get references to the heart and transparent heart images
+const heartImg = document.getElementById('heart');
+const transparentHeartImg = document.getElementById('heart-transparent');
 
-    // Simulate a click on the link to trigger the download
-    link.click();
+// Add event listeners to the transparent heart image
+transparentHeartImg.addEventListener('mouseover', () => {
+  // Apply the hover effect by changing the scale of the heart image
+  heartImg.style.transform = 'scale(1.1)';
 });
 
-
-
-
-
-
-
-
-
-
-
-
+transparentHeartImg.addEventListener('mouseout', () => {
+  // Remove the hover effect by resetting the scale of the heart image
+  heartImg.style.transform = 'scale(1)';
+});
